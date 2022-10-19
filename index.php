fe9caea34831ea2e9afe31e22a747e5c102432af
Komlev Ivan <komleviv@gmail.com>
2022-10-19 14:51:54 +0600

    Added display of post view

--- index.php
+++ index.php
@@ -24,7 +24,12 @@ foreach(array_reverse($messages) as $i => $message) {
  if (isset($message['entities'][0]['url'])) {
    $source .= "<div class='message_url'><a href='". $message['entities'][0]['url'] ."' target='_blank'>" . $message['entities'][0]['url']. "</a></div>";
  }
- $source .= "<div class='message_bottom'><div class='bottom_block'><a href='https://t.me/btca_ru/" . $message['id'] ."' target='_blank'>t.me/btca_ru/" . $message['id'] ."</a></div><div class='bottom_block><span class='message_date'>" . date('j F Y H:i', $message['date']) ."</span></div></div>";
+ $source .= "<div class='message_bottom'><div class='bottom_block'><a href='https://t.me/btca_ru/" . $message['id'] ."' target='_blank'>t.me/btca_ru/" . $message['id'] ."</a></div><div class='bottom_block_right'><div class='message_view'>" . $message['views'] . "<svg class='view_icon' version='1.1' id='Layer_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px'
+	 viewBox='0 0 42 42' enable-background='new 0 0 42 42' xml:space='reserve'>
+<path d='M15.3,20.1c0,3.1,2.6,5.7,5.7,5.7s5.7-2.6,5.7-5.7s-2.6-5.7-5.7-5.7S15.3,17,15.3,20.1z M23.4,32.4
+	C30.1,30.9,40.5,22,40.5,22s-7.7-12-18-13.3c-0.6-0.1-2.6-0.1-3-0.1c-10,1-18,13.7-18,13.7s8.7,8.6,17,9.9
+	C19.4,32.6,22.4,32.6,23.4,32.4z M11.1,20.7c0-5.2,4.4-9.4,9.9-9.4s9.9,4.2,9.9,9.4S26.5,30,21,30S11.1,25.8,11.1,20.7z'/>
+</svg></div><span>" . date('j F Y H:i', $message['date']) ."</span></div></div>";
  $source .= "</div>";
 }
 
